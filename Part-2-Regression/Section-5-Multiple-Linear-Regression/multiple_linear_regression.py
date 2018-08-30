15d5564f0027d77d96acfd3e5be5757f8d512528
tambulkar <tanish.ambulkar99@gmail.com>
2018-08-29 22:53:37 -0700

    finished multiple linear regression model

--- Part-2-Regression/Section-5-Multiple-Linear-Regression/multiple_linear_regression.py
+++ Part-2-Regression/Section-5-Multiple-Linear-Regression/multiple_linear_regression.py
@@ -11,26 +11,20 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
-# Avoiding the Dummy Variable Trap
+# Dummy Variable Trap
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# Splitting the dataset into Training and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -39,3 +33,33 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr=np.ones(shape=[50,1]).astype(int), values=X, axis=1)
+# X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# regressor_OLS.summary()
+# X_opt = X[:, [0, 1, 3, 4, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# regressor_OLS.summary()
+# X_opt = X[:, [0, 3, 4, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# regressor_OLS.summary()
+# X_opt = X[:, [0, 3, 5]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# regressor_OLS.summary()
+# X_opt = X[:, [0, 3]]
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(numVars):
+        regressor_OLS = sm.OLS(endog=y, exog=x).fit()
+        maxPVal = max(regressor_OLS.pvalues).astype(float)
+        if (maxPVal > sl):
+            for j in range(numVars - i):
+                if(regressor_OLS.pvalues[j].astype(float) == maxPVal):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+X_opt = backwardElimination(X, 0.05)
